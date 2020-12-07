4d8e92bc19ecbf0002acf6abded74e11eeb22b79
Yoshiyasu Takefuji <ytakefuji@gmail.com>
2020-12-07 12:54:54 +0900

    Add files via upload

---
+++ daily.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+data=pd.read_csv("euro.csv")
+country=input("enter country ")
+n=len(data[country])
+days1=255
+y=data[country][n-days1:n]
+x=np.arange(n-days1,n)
+valid = ~(np.isnan(x) | np.isnan(y))
+degree1=10
+model=np.poly1d(np.polyfit(x[valid],y[valid],degree1))
+date=data['date'][n-1]
+x1=np.arange(n-days1,n+7)
+y1=model(x1)
+plt.plot(x,y,'k')
+plt.plot(x1,y1,'b')
+days2=100
+y=data[country][n-days2:n]
+x=np.arange(n-days2,n)
+import numpy as np
+valid = ~(np.isnan(x) | np.isnan(y))
+degree2=5
+model=np.poly1d(np.polyfit(x[valid],y[valid],degree2))
+x1=np.arange(n-days2,n+7)
+y1=model(x1)
+plt.plot(x1,y1,'r')
+plt.legend(['daily deaths in '+str(country),str(days1)+' days from '+str(date)+' '+str(degree1)+'th',str(days2)+' days from '+str(date)+' '+str(degree2)+'th'])
+plt.show()
